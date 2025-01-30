18085c4562c89a79d1effe7df4b40f901a835ce7
oe1307 <oe.issa.392@gmail.com>
2025-01-30 11:22:00 +0900

    update

--- config/nvim/lua/plugin/lsp.lua
+++ config/nvim/lua/plugin/lsp.lua
@@ -48,7 +48,6 @@ return {
         build = ":TSUpdate",
         config = function()
             require("nvim-treesitter.configs").setup({
-                ensure_installed = { "python" },
                 highlight = { enable = true },
                 indent = { enable = true },
                 autotag = { enable = true },
