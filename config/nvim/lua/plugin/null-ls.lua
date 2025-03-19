3158e8bfce615258699bf457d1bf51fd69a453ad
oe1307 <oe.issa.392@gmail.com>
2025-03-19 12:18:25 +0900

    update

--- config/nvim/lua/plugin/null-ls.lua
+++ config/nvim/lua/plugin/null-ls.lua
@@ -1,5 +1,14 @@
 return {
     {
+        "rachartier/tiny-inline-diagnostic.nvim",
+        event = "VeryLazy",
+        priority = 1000,
+        config = function()
+            require("tiny-inline-diagnostic").setup()
+            vim.diagnostic.config({ virtual_text = false })
+        end,
+    },
+    {
         "jay-babu/mason-null-ls.nvim",
         lazy = false,
         keys = {
