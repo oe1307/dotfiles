018f1a1531798611ffc12253add74810feef350d
oe1307 <oe.issa.392@gmail.com>
2025-04-12 17:43:15 +0900

    update

--- config/nvim/lua/plugin/lsp.lua
+++ config/nvim/lua/plugin/lsp.lua
@@ -1,41 +1,6 @@
 return {
     {
-        "williamboman/mason-lspconfig.nvim",
-        lazy = false,
-        keys = {
-            { "K", ":lua vim.lsp.buf.definition()<Return>", silent = true },
-            { "re", ":lua vim.lsp.buf.rename()<Return>", silent = true },
-        },
-        dependencies = {
-            "neovim/nvim-lspconfig",
-            "williamboman/mason.nvim",
-            "hrsh7th/cmp-nvim-lsp",
-        },
-        config = function()
-            require("mason").setup()
-            require("mason-lspconfig").setup({})
-            require("mason-lspconfig").setup_handlers({
-                function(server)
-                    require("lspconfig")[server].setup({})
-                end,
-                jedi_language_server = function()
-                    require("lspconfig").jedi_language_server.setup({
-                        capabilities = require("cmp_nvim_lsp").default_capabilities(),
-                    })
-                end,
-                clangd = function()
-                    require("lspconfig").clangd.setup({
-                        init_options = {
-                            compilationDatabasePath = ".build",
-                        },
-                        capabilities = require("cmp_nvim_lsp").default_capabilities(),
-                        cmd = { "clangd", "--offset-encoding=utf-16" },
-                    })
-                end,
-            })
-        end,
-    },
-    {
+        -- syntax highlighting
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function()
