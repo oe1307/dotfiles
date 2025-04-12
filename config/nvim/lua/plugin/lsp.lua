ec74b7b8e40667800bf87fb999b015f7ddd261be
oe1307 <oe.issa.392@gmail.com>
2025-04-12 18:59:41 +0900

    update

--- config/nvim/lua/plugin/lsp.lua
+++ config/nvim/lua/plugin/lsp.lua
@@ -1,5 +1,14 @@
 return {
     {
+        -- LSP configuration
+        "neovim/nvim-lspconfig",
+        config = function()
+            vim.keymap.set("n", "K", vim.lsp.buf.definition, { buffer = bufnr })
+            local lspconfig = require("lspconfig")
+            lspconfig.pyright.setup({})
+        end,
+    },
+    {
         -- syntax highlighting
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
