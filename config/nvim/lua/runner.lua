aad8f8072664c33a08e14a68875ce98f31d82c99
oe1307 <oe.issa.392@gmail.com>
2025-06-10 13:54:40 +0900

    update

--- config/nvim/lua/runner.lua
+++ config/nvim/lua/runner.lua
@@ -1,13 +1,13 @@
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "python",
     callback = function()
-        vim.keymap.set("n", "R", ":belowright split | term python %<Enter>i")
+        vim.keymap.set("n", "R", ":belowright split | term python %<Enter>i", silent=true)
     end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "python",
     callback = function()
-        vim.keymap.set("n", "<C-s>", "o# -- DEBUG --<Enter>print()<Enter># -- End --<Enter><Esc>kk$i\"\"<Left>")
+        vim.keymap.set("n", "<C-s>", "o# breakpoint()<esc>", silent=true)
     end,
 })
