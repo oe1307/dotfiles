c8abfd052a54ef69d4696b9430413e6bbd76361c
oe1307 <oe.issa.392@gmail.com>
2025-12-04 18:26:12 +0900

    update

--- config/nvim/lua/autocmd.lua
+++ config/nvim/lua/autocmd.lua
@@ -20,3 +20,30 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
         end
     end,
 })
+
+-- marp
+local marp_augroup = vim.api.nvim_create_augroup("MarpPreviewGroup", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+    group = marp_augroup,
+    pattern = "markdown",
+    callback = function()
+        if vim.bo.buftype ~= "" or vim.fn.empty(vim.fn.expand("%")) == 1 then
+            return
+        end
+        local command_args = {
+            vim.fn.expand("%"),
+            "-p",
+            "--theme",
+            vim.fn.expand("~/dotfiles/config/marp/ntt.css"),
+        }
+
+        vim.loop.spawn("marp", {
+            args = command_args,
+            cwd = vim.fn.fnamemodify(vim.fn.expand("%"), ":h"),
+        }, function(exit_code, signal)
+            if exit_code ~= 0 then
+                vim.notify("Marp Preview failed (Exit Code: " .. exit_code .. ")", vim.log.levels.ERROR)
+            end
+        end)
+    end,
+})
