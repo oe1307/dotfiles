f7bbd1db724a9d27eb31f0de68644d5ff613201b
Issa Oe <77875118+oe1307@users.noreply.github.com>
2025-12-04 14:17:47 +0900

    Update Microsoft.PowerShell_profile.ps1

--- config/powershell/Microsoft.PowerShell_profile.ps1
+++ config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,12 @@
 Set-PSReadlineKeyHandler -Key ctrl+d -Function DeleteCharOrExit
 
 Set-Alias vim nvim
+function cd-home { cd ~ }
+Set-Alias -Name cdr -Value cd-home
 
 del alias:gp -Force
 function git-pull { git pull $args }
 Set-Alias -Name gp -Value git-pull
+
+function git-status { git status $args }
+Set-Alias -Name gs -Value git-status
