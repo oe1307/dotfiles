9c5f4ad01906c5129c39bca9025f4ee4ebf737cc
oe1307 <oe.issa.392@gmail.com>
2025-12-13 20:00:13 +0900

    update

--- config/macos/setup.sh
+++
@@ -1,3 +0,0 @@
-defaults write com.apple.screencapture location -string /Users/issa/Downloads
-defaults write com.apple.screencapture type -string "png"
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
