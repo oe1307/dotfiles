69ab61f242727a8560978b731b1637f4be2a9bf4
oe1307 <oe.issa.392@gmail.com>
2025-12-26 14:19:05 +0900

    update

--- config/nvim/lua/plugin/cmp.lua
+++ config/nvim/lua/plugin/cmp.lua
@@ -44,7 +44,7 @@ return {
                 }),
                 mapping = cmp.mapping.preset.insert({
                     ["<Tab>"] = cmp.mapping.select_next_item(),
-                    ["<S-Tab"] = cmp.mapping.select_prev_item(),
+                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
                 }),
             })
 
