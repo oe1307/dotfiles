8f4a5bde3ed477bbdedb03243069d4b65763fddf
oe1307 <oe.issa.392@gmail.com>
2026-02-01 10:47:14 +0900

    • chore(nvim): simplify options and todo config
      Remove the ambiwidth option and reset todo-comments
      configuration to default options for simpler maintenance.

--- config/nvim/lua/packages.lua
+++ config/nvim/lua/packages.lua
@@ -24,17 +24,7 @@ return {
         "folke/todo-comments.nvim",
         event = "VeryLazy",
         dependencies = "nvim-lua/plenary.nvim",
-        opts = {
-            keywords = {
-                FIX = { icon = "" },
-                TODO = { icon = "" },
-                HACK = { icon = "" },
-                WARN = { icon = "" },
-                PERF = { icon = "" },
-                NOTE = { icon = "" },
-                TEST = { icon = "⏲" },
-            },
-        },
+        opts = {},
     },
     {
         -- multiple cursors
