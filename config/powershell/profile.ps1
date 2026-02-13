7028ec5a340075e6428e65802a61df691a7b0575
oe1307 <oe.issa.392@gmail.com>
2026-02-13 17:00:15 +0900

    update

--- config/powershell/profile.ps1
+++ config/powershell/profile.ps1
@@ -1,7 +1,7 @@
 cd $HOME/work
 
 $env:XDG_CONFIG_HOME = "$HOME\.config"
-$env:GIT_SSH_COMMAND = "C:/Windows/System32/OpenSSH/ssh.exe -F $HOME/.ssh/config"
+$env:GIT_SSH_COMMAND = '"C:\Windows\System32\OpenSSH\ssh.exe" -F "$HOME\.ssh\config"'
 
 Set-PSReadlineKeyHandler -Key ctrl+d -Function DeleteCharOrExit
 
