b9de4e403b9955bbf4367ba5d0fc02fa800f8ee1
oe1307 <oe.issa.392@gmail.com>
2026-07-22 12:59:19 +0900

    initial commit

---
+++ setup.ps1
@@ -0,0 +1,24 @@
+New-Item -ItemType Directory -Force -Path (Split-Path $PROFILE) | Out-Null
+Set-Content -Path $PROFILE -Value ". `"$HOME\dotfiles\shell\profile.ps1`""
+
+# ssh keys
+$path = "$HOME\.ssh\key\github.pem"
+(Get-Content -Raw $path) -replace "`r`n","`n" | Set-Content -NoNewline $path
+
+# dotfiles
+New-Item -ItemType Directory -Force -Path "$HOME\.config" | Out-Null
+
+$baseSource = Join-Path $HOME "dotfiles\config"
+$baseDest   = Join-Path $HOME ".config"
+
+Get-ChildItem -LiteralPath $baseSource -Directory | ForEach-Object {
+    $name       = $_.Name
+    $sourcePath = $_.FullName
+    $destPath   = Join-Path $baseDest $name
+
+    if (Test-Path -LiteralPath $destPath) {
+        Remove-Item -LiteralPath $destPath -Recurse -Force
+    }
+
+    Copy-Item -LiteralPath $sourcePath -Destination $destPath -Recurse -Force
+}
