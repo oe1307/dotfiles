521f3d32729a3ec66774b1943c91a2314afc9f99
oe1307 <oe.issa.392@gmail.com>
2026-08-20 10:50:38 +0900

    chore(windows-terminal): add sync scripts
    Replace the README copy command with dedicated PowerShell scripts.
    
    Add backup.ps1 to save Windows Terminal settings to dotfiles and
    update.ps1 to restore the tracked settings to Windows Terminal.

---
+++ config/windows_terminal/backup.ps1
@@ -0,0 +1,1 @@
+cp -Force ~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json ~\dotfiles\config\windows_terminal\settings.json
---
+++ config/windows_terminal/update.ps1
@@ -0,0 +1,1 @@
+cp -Force ~\dotfiles\config\windows_terminal\settings.json ~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
